1553e03d8e51e035edebea47c7ae15554d730b2f
NotWhat51 <sofiabelsk51@gmail.com>
2023-04-05 19:31:45 +0300

    module2-task1

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const PromoFilm = {
+  nameFilm: 'A Clockworkk Orange',
+  genreFilm: 'fantasy',
+  releaseYearFilm: 1971
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App nameFilm={PromoFilm.nameFilm} genreFilm={PromoFilm.genreFilm} releeseYearFilm={PromoFilm.releaseYearFilm} />
   </React.StrictMode>,
 );
