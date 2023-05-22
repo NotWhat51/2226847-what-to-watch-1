a68ab738bb62627424c13b2eba421181b2d8b2a7
NotWhat51 <sofiabelsk51@gmail.com>
2023-05-22 16:18:49 +0300

    module5-task1

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,21 +1,40 @@
 import {Link} from 'react-router-dom';
 import FilmType from '../../types/film-type';
+import {useEffect, useState} from 'react';
+import { Player } from '../player/player';
 
 type FilmCardProps = {
     film: FilmType;
     setActiveFilmCard: (id: number) => void;
+    isActive: boolean;
 }
 
-export const FilmCard = (props: FilmCardProps): JSX.Element => (
-  <article className="small-film-card catalog__films-card"
-    onMouseEnter={() => props.setActiveFilmCard(props.film.id)}
-    onMouseLeave={() => props.setActiveFilmCard(NaN)}
-  >
-    <div className="small-film-card__image">
-      <img src={props.film.posterImage} alt={props.film.name} width="280" height="175" />
-    </div>
-    <h3 className="small-film-card__title">
-      <Link to = {`/film/${props.film.id}`} className="small-film-card__link">{props.film.name}</Link>
-    </h3>
-  </article>
-);
+export const FilmCard = (props: FilmCardProps): JSX.Element => {
+  const [isPlaying, setIsPlaying]= useState(false);
+  useEffect (() => {
+    if (props.isActive) {
+      const timer = setTimeout(() => setIsPlaying(true), 1000);
+      return () => {
+        clearTimeout(timer);
+        setIsPlaying(false);
+      };
+    }
+  }, [props.isActive]);
+
+  return (
+    <article className="small-film-card catalog__films-card"
+      onMouseEnter={() => props.setActiveFilmCard(props.film.id)}
+      onMouseLeave={() => props.setActiveFilmCard(NaN)}
+    >
+      <div className="small-film-card__image">
+        {isPlaying
+          ? <Player videoSrc={props.film.previewVideolink} posterImageSrc={props.film.posterImage} muted/>
+          : <img src={props.film.posterImage} alt={props.film.name} width="280" height="175"/>
+        }
+      </div>
+      <h3 className="small-film-card__title">
+        <Link to = {`/film/${props.film.id}`} className="small-film-card__link">{props.film.name}</Link>
+      </h3>
+    </article>
+  );
+};
